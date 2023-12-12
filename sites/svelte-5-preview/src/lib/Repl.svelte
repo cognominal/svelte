a7f01e8a19c63e518be32587787b74e516d2d551
Stéphane Payrard <cognominal@gmail.com>
2023-12-12 20:04:06 +0100

    supress a console.log()

--- sites/svelte-5-preview/src/lib/Repl.svelte
+++ sites/svelte-5-preview/src/lib/Repl.svelte
@@ -15,7 +15,6 @@
 
 	export let packagesUrl = 'https://unpkg.com';
 	export let svelteUrl = `${BROWSER ? location.origin : ''}/svelte`;
-	console.log(`svelteUrl ${BROWSER} ${svelteUrl}`);
 	export let embedded = false;
 	/** @type {'columns' | 'rows'} */
 	export let orientation = 'columns';

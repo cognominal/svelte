3c30f9afbae91c528f8e79ebf350aa4d24a34905
Stéphane Payrard <cognominal@gmail.com>
2024-12-03 21:34:45 +0100

    [/RouteExplorer] handling of up/down arrow
    CSS not quite right

--- apps/svelte.dev/src/lib/components/filetree/FileTree.svelte
+++ apps/svelte.dev/src/lib/components/filetree/FileTree.svelte
@@ -6,10 +6,51 @@
 
     interface Props {
         folder: IFolder ;
+        mobile?: boolean;
+
     }
 
-    let { folder }: Props = $props();
+    let { folder, mobile = false }: Props = $props();
 
 </script>
 
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+
+<ul
+	class="filetree"
+	class:mobile
+	onkeydown={(e) => {
+		if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+			e.preventDefault();
+			const lis = Array.from(e.currentTarget.querySelectorAll('li'));
+			const focused = lis.findIndex((li) => li.contains( (e.target as HTMLElement)));
+
+			const d = e.key === 'ArrowUp' ? -1 : +1;
+
+			lis[focused + d]?.querySelector('button')?.focus();
+		}
+	}}
+>
 <Folder prefix='' {folder}/>
+</ul>
+
+<style>
+	.filetree {
+		font: var(--sk-font-ui-small);
+		flex: 1;
+		overflow-y: auto;
+		overflow-x: hidden;
+		padding: 1rem 0rem;
+		margin: 0;
+		background: var(--sk-bg-3);
+		list-style: none;
+	}
+
+	.filetree.mobile {
+		height: 100%;
+	}
+
+	.modal-contents p {
+		white-space: pre-line;
+	}
+</style>
